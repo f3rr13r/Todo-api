82965b841b4e323563177f0b03fae9d0c311285d
Harry Ferrier <f3rr13r@hotmail.com>
2016-09-07 15:24:31 +0100

    Add /todos and /todos/:id

--- server.js
+++ server.js
@@ -1,6 +1,25 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [{
+	id: 1,
+	description: 'Do Laundry',
+	completed: false
+	},
+	{
+	id: 2,
+	description: 'Email Kevin Seal',
+	completed: false
+	},
+	{
+	id: 3,
+	description: 'Cook Dinner',
+	completed: false
+	}
+	];
+
+
+
 
 app.get('/', function (request, response) {
 
@@ -8,8 +27,61 @@ app.get('/', function (request, response) {
 
 });
 
+
+
+
+// GET /todos
+app.get('/todos', function (request, response) {
+
+	response.json(todos);
+
+});
+
+
+
+
+// GET /todos/:<your todo id here>
+app.get('/todos/:id', function (request, response) {
+
+	var todoID = request.params.id;
+	var matchedItem;
+
+	// Iterate through each of the todo object models.
+	todos.forEach(function (todo) {
+
+		if (todo.id == todoID) {
+
+			matchedItem = todo;
+
+		}
+
+	});
+
+	if (typeof matchedItem === 'undefined') {
+
+		response.status(404).send();
+
+	} else {
+
+		response.json(matchedItem);
+
+	}
+
+});
+
+
+
+
 app.listen(PORT, function () {
 
 	console.log('Express listening on port ' + PORT + '.');
 
 });
+
+
+
+
+
+
+
+
