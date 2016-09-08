d67a4859eaeb9ceb80a993533fa0840c15adb4e0
Harry Ferrier <f3rr13r@hotmail.com>
2016-09-08 15:03:01 +0100

    Add completed filter

--- server.js
+++ server.js
@@ -28,10 +28,21 @@ app.get('/', function (request, response) {
 
 
 
-// GET /todos
+// GET /todos?completed=true
 app.get('/todos', function (request, response) {
 
-	response.json(todos);
+	var queryParams = request.query;
+	var filteredTodos = todos;
+
+	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+		filteredTodos = _.where(filteredTodos, {completed: true});
+
+	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+		filteredTodos = _.where(filteredTodos, {completed: false});
+
+	}
+
+	response.json(filteredTodos);
 
 });
 
