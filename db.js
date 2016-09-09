8d13f1e3ce705862859f7b56bfdd40c0322c8d84
Harry Ferrier <f3rr13r@hotmail.com>
2016-09-09 09:42:40 +0100

    Rectified pg version issues

--- db.js
+++ db.js
@@ -6,14 +6,13 @@ if (env === 'production') {
 
 	sequelize = new Sequelize(process.env.DATABASE_URL, {
 		dialect: 'postgres'
-
 	});
 
 } else {
 
 	sequelize = new Sequelize(undefined, undefined, undefined, {
 		'dialect': 'sqlite',
-		'storage': __dirname + '/data/dev-todo-api.sqlite'
+		'storage': __dirname + '/Data/dev-todo-api.sqlite'
 
 	});
 
@@ -22,7 +21,7 @@ if (env === 'production') {
 
 var db = {};
 
-db.todo = sequelize.import(__dirname + '/models/todo.js');
+db.todo = sequelize.import(__dirname + '/Models/todo.js');
 
 db.sequelize = sequelize;
 
